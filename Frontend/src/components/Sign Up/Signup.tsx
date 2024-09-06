f03513ffef06a078cfcca52529a5e261de397d44
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-09-06 18:59:03 +0530

    Sign up page is completed

--- Frontend/src/components/Sign Up/Signup.tsx
+++ Frontend/src/components/Sign Up/Signup.tsx
@@ -1,21 +1,47 @@
+import {Link} from "react-router-dom"
 
 function Signup() {
   return (
     <>
         <div className="w-full h-screen flex justify-center items-center">
-            <div className="w-96">
+            <div className="w-96 flex flex-col gap-4">
                 <div className="text-center">
-                    <h1>shopi</h1>
+                    <h1 className="text-3xl font-bold">shopi</h1>
                 </div>
                 <div className="border-solid border border-black rounded-md p-3">
-                    <h2 className="text-xl mb-4">Create Account</h2>
-                    <div className="flex flex-col gap-1">
-                        <label>Your name</label>
-                        <input 
-                            type="text"
-                            placeholder="First name and last name"
-                            className="border border-solid border-black px-2 py-1 rounded-md  shadow-md placeholder:text-sm outline-none"
-                        />
+                    <form>
+                        <h2 className="text-2xl mb-4">Create Account</h2>
+                        <div className="flex flex-col gap-1 mb-4">
+                            <label className="text-text">Your name</label>
+                            <input 
+                                type="text"
+                                placeholder="First name and last name"
+                                className="border border-solid border-black px-2 py-2 text-text text-sm rounded-md shadow-sm shadow-primary placeholder:text-sm outline-none transition-all duration-300 focus:ring-1 focus:ring-attention"
+                            />
+                        </div>
+                        <div className="flex flex-col gap-1 mb-4">
+                            <label className="text-text">Mobile number</label>
+                            <input 
+                                type="text"
+                                placeholder="Mobile number"
+                                className="border border-solid border-black px-2 py-2 text-text text-sm rounded-md shadow-sm shadow-primary placeholder:text-sm outline-none transition-all duration-300 focus:ring-1 focus:ring-attention"
+                            />
+                        </div>
+                        <div className="flex flex-col gap-1 mb-4">
+                            <label className="text-text">Password</label>
+                            <input 
+                                type="password"
+                                placeholder="At least 6 characters"
+                                className="border border-solid border-black px-2 py-2 text-text text-sm rounded-md shadow-sm shadow-primary placeholder:text-sm outline-none transition-all duration-300 focus:ring-1 focus:ring-attention"
+                            />
+                            <p className="text-text text-xs my-3">To verify your number, we will send you a text message with a temporary code. Message and data rate may apply.</p>
+                        </div>
+
+                        <button type="submit" className="bg-cta text-text font-semibold w-full py-2 rounded-md hover:bg-ctah">Verify mobile number</button>
+                    </form>
+                    <div className="mt-8 mb-2 flex justify-between">
+                        <p className="font-bold text-sm text-text">Already have an account?</p>
+                        <Link to={`/signin`} className="text-cta text-sm font-bold hover:underline">Sign In</Link>
                     </div>
                 </div>
             </div>
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,17 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      primary: "#003b36",
+      cta: "#e98a15",
+      ctah: "#fa9e2d",
+      bg: "#ece5f0",
+      discount: "#eb0a0a",
+      attention: "#59114d",
+      text: "#012622",
+      hbg: "#003b36",
+      helement: "#ece5f0"
+    },
     extend: {
-      custom: {
-        primary: "#003b36",
-        cta: "#e98a15",
-        bg: "#ece5f0",
-        discount: "#eb0a0a",
-        attention: "#59114d",
-        text: "#012622",
-        hbg: "#003b36",
-        helement: "#ece5f0"
-      }
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
     },
   },
   plugins: [],
