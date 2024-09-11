4b6072ad1ad339b1851fb6f8e3b2af15ae4575d4
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-09-11 06:32:17 +0530

    First half of single product is completed

--- Frontend/src/components/productDetails/actionButton/ActionButton.tsx
+++ Frontend/src/components/productDetails/actionButton/ActionButton.tsx
@@ -2,7 +2,14 @@
 
 function ActionButton() {
   return (
-    <div>ActionButton</div>
+    <div className="w-1/2 p-2 float-right flex justify-between">
+      <div>
+        <button className="px-8 py-2 bg-bg text-text font-semibold hover:bg-bgh rounded-md ">Add to cart</button>
+      </div>
+      <div>
+        <button className="px-8 py-2 bg-cta hover:bg-ctah text-text font-semibold rounded-md">Buy Now</button>
+      </div>
+    </div>
   )
 }
 
