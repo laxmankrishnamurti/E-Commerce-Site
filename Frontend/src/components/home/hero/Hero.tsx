199f4bc7e2170b38b1d3d84513d533d044098d00
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-09-12 22:04:44 +0530

    Trying to change height of the hero image

--- Frontend/src/components/home/hero/Hero.tsx
+++ Frontend/src/components/home/hero/Hero.tsx
@@ -2,7 +2,7 @@ import heroImage from '/cart-hero.jpg';
 
 function Hero() {
   return (
-    <div className="w-full">
+    <div className="w-full h-1/4">
         <img src={heroImage} className="w-full h-full"/>
     </div>
   )
