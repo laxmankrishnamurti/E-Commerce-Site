1ff4ab81df6672fe597c86618b00fa4625824f0e
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-09-14 20:03:19 +0530

    Working on admin panel / seller website

--- seller/src/App.tsx
+++ seller/src/App.tsx
@@ -3,7 +3,7 @@
 function App() {
   return (
     <>
-      <h1 className='bg-orange-500 p-4 text-center font-semibold'>This is admin panel</h1>
+      <h1 className='bg-orange-500 p-4 text-center'>This is admin panel</h1>
     </>
   )
 }
