c3a913656a202c755ce0b57d3e40428a19fc5542
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-09-14 20:01:50 +0530

    Working on admin panel / seller website

---
+++ seller/src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-orange-500 p-4 text-center font-semibold'>This is admin panel</h1>
+    </>
+  )
+}
+
+export default App
