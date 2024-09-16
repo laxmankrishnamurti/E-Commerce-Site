898043147949d525991416c0d73018349cf96126
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-09-16 20:24:58 +0530

    Adding a button to delete cart product if quantity will be 1

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from "express";
 require("dotenv").config();
+import express, { Request, Response } from "express";
 import config from "./config/config";
 import connectToDatabase from "./database/connectToDatabase";
 
