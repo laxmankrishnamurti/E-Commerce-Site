aef41fcd03d7efb9d805562436c260762ecd9053
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-09-17 23:37:35 +0530

    Trying to create an asynchandler function

---
+++ Backend/src/utils/asyncHandler.ts
@@ -0,0 +1,5 @@
+// import { Request, Response, NextFunction } from "express";
+
+// const asyncHandler = (fun) => {
+//   return (Promise.resolve(fun(req: Request, res: Response, next: NextFunction)).catch((error) => next(error)));
+// };
