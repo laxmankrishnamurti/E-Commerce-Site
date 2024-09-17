617bc9a902baae60ac1405c6803b836b90dfe529
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-09-17 20:39:23 +0530

    Understanding about Errors

--- Learning Curve From The Project/NodeJs/Errors/index.js
+++ Learning Curve From The Project/NodeJs/Errors/index.js
@@ -1,17 +1,14 @@
 async function sayHello() {
   try {
-    throw new Error("sayHello fun :: Program failed");
+    throw new Error(
+      "sayHello fun :: Program failed",
+      "Database insertion failed"
+    );
   } catch (error) {
     console.log("Error : ", error);
     console.log("Error name : ", error.name);
     console.log("Error message : ", error.message);
     console.log("Error stack : ", error.stack);
-    let allErrorKeys = [];
-    for (let key in error) {
-      allErrorKeys.push(key);
-    }
-    console.log("allErrorKeys are : ", allErrorKeys);
   }
 }
-
 sayHello();
