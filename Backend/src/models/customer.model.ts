e5c27d16a496af0c4fcec47c8ef42a6dc6e89daa
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-09-18 18:02:18 +0530

    Working on model validation

--- Backend/src/models/customer.model.ts
+++ Backend/src/models/customer.model.ts
@@ -4,30 +4,54 @@ const customerSchema = new mongoose.Schema(
   {
     fullName: {
       type: String,
-      required: true,
+      required: [true, "Fullname is required"],
     },
     email: {
       type: String,
-      required: true,
+      required: [true, "Email is required"],
+      unique: true,
     },
     phoneNumber: {
       type: Number,
-      required: true,
+      required: [true, "Phone number is required"],
     },
     password: {
       type: String,
+      min: [6, "Password atleast have 6 characters"],
+      max: 12,
       required: true,
     },
     shippingAddress: [
       {
-        fullName: { type: String },
-        mobileNumber: { type: Number },
-        alternateNumber: { type: Number },
-        streetAddress: { type: String },
-        landMark: { type: String },
-        pinCode: { type: Number },
-        city: { type: String },
-        state: { type: String },
+        fullName: {
+          type: String,
+          required: true,
+        },
+        mobileNumber: {
+          type: Number,
+          required: true,
+        },
+        alternateNumber: {
+          type: Number,
+        },
+        streetAddress: {
+          type: String,
+        },
+        landMark: {
+          type: String,
+        },
+        pinCode: {
+          type: Number,
+          required: true,
+        },
+        city: {
+          type: String,
+          required: true,
+        },
+        state: {
+          type: String,
+          required: true,
+        },
       },
     ],
     cartProducts: [
@@ -35,14 +59,22 @@ const customerSchema = new mongoose.Schema(
         productId: {
           type: Schema.Types.ObjectId,
           ref: "products",
+          required: [
+            true,
+            "Product id is required to add products into cart product",
+          ],
         },
       },
     ],
-    orders: [
+    orderHistory: [
       {
         productId: {
           type: Schema.Types.ObjectId,
           ref: "products",
+          required: [
+            true,
+            "Product id is required to add products into order histroy",
+          ],
         },
       },
     ],
