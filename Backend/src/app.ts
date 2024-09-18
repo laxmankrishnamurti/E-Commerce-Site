aaeb2b635fbfb166e7d21bbb867ab416492877da
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-09-18 21:55:30 +0530

    Whitelisting the frontend doamin to make a request to the server

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,6 +1,14 @@
 import express, { Application } from "express";
+import cors from "cors";
 
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(
   express.json({
