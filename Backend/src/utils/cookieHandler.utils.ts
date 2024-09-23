3d402081407a3943f0f9f4ad65a986cfd6a3f1d7
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-09-23 10:38:48 +0530

    Adding token verification logic

--- Backend/src/utils/cookieHandler.utils.ts
+++ Backend/src/utils/cookieHandler.utils.ts
@@ -1,19 +1,41 @@
-import jsonwebtoken from "jsonwebtoken"
-import config from "../config/config.ts"
+import jsonwebtoken from "jsonwebtoken";
+import config from "../config/config.ts";
+import CustomErrorClass from "./customErrorClass.utils.ts";
 
 interface IPayload {
-    userId: string
+  userId: string;
 }
 
 const generateToken = (payload: IPayload): string => {
-    const generatedToekn: string = jsonwebtoken.sign(
-        payload, 
-        config.jsonwebtoken_secret,
-        {expiresIn: "1d"}
-    )
-    return generatedToekn
-}
+  try {
+    const generatedTokenn: string = jsonwebtoken.sign(
+      payload,
+      config.jsonwebtoken_secret,
+      { expiresIn: "1d" }
+    );
+    return generatedTokenn;
+  } catch (error) {
+    throw new CustomErrorClass(500, "Internal server error");
+  }
+};
 
-export {
-    generateToken
-}
+const verifyToken = (token: string): IPayload => {
+  try {
+    const result = jsonwebtoken.verify(token, config.jsonwebtoken_secret);
+
+    if (typeof result === "string") {
+      throw new CustomErrorClass(400, "Invalid token format");
+    }
+
+    return result as IPayload;
+  } catch (error) {
+    if (error instanceof jsonwebtoken.JsonWebTokenError) {
+      throw new CustomErrorClass(401, "Invalid or expired token");
+    } else {
+      // Rethrow any other errors (possibly internal server errors)
+      throw error;
+    }
+  }
+};
+
+export { generateToken, verifyToken };
